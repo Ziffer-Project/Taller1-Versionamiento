fba234ca32dcf3ac9251fd54e2501e8ec8f7466f
Daviddager <davdag7@gmail.com>
2015-03-17 00:24:42 -0500

    Clase Admin agregada con setters y getters

---
+++ src/Admin.java
@@ -0,0 +1,23 @@
+import java.util.*
+
+public class Admin{
+
+  private int level;
+  private rating double;
+
+  public void setLevel( int level ){
+    this.level = level;
+  }
+
+  public int getLevel( ){
+    return level;
+  }
+
+  public void setRating( double rating ){
+    this.rating = rating;
+  }
+
+  public double getRating( ){
+    return rating;
+  }
+}
