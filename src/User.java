c19cdfa6931ed46c78abe61e6c20acd588598a28
dolopezo <dolopezo@unal.edu.co>
2015-03-17 20:43:12 -0500

    -Añadida clase user

--- src/User.java
+++ src/User.java
@@ -16,37 +16,6 @@ public class User {
         this.name=name;
     }
 
-    public String getLastName(){
-        return lastname;
-    }
-
-    public void setLastName(String lastname){
-        this.lastname=lastname;
-    }
-
-    public int getAge(){
-        return age;
-    }
-
-    public void setAge(int age){
-        this.age=age;///
-    }
-
-    public String getUsername(){
-        return username;
-    }
-
-    public void setUsername(String username){
-        this.username=username;
-    }
-
-    public String getPassword(){
-        return password;
-    }
-
-    public void setPassword(String password){
-        this.password=password;
-    }
 
 
 }
