a13ea96ae1f8d0b232f540b6252e85a9cbdbd7cf
Daniel Aranda <dwares2008@hotmail.com>
2015-03-26 14:53:14 -0500

    Modificada herencia y relaciones de la clase de acuerdo a diagrama UML

--- src/Admin.java
+++ src/Admin.java
@@ -1,23 +1,32 @@
-import java.util.*
+import java.util.*;
 
-public class Admin{
+public class Admin extends User {
 
-  private int level;
-  private double rating;
+	private int level;
+	private double rating;
+	private List<Forum> forums = new ArrayList<Forum>();
 
-  public void setLevel( int level ){
-    this.level = level;
-  }
+	public void setLevel(int level) {
+		this.level = level;
+	}
 
-  public int getLevel( ){
-    return level;
-  }
+	public int getLevel() {
+		return level;
+	}
 
-  public void setRating( double rating ){
-    this.rating = rating;
-  }
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
 
-  public double getRating( ){
-    return rating;
-  }
+	public double getRating() {
+		return rating;
+	}
+	
+	public void setForums(List<Forum> forums){
+		this.forums = forums;
+	}
+	
+	public List<Forum> getForums(){
+		return forums;
+	}
 }
